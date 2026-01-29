5467ec3fb94c5efb45bbf4b9f6d61422e84a877d
David <david@appliedautonomics.com>
2026-01-28 21:39:28 -0600

    more static fixes

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -151,10 +151,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "frontend" / "assets",
-]
-
 STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
